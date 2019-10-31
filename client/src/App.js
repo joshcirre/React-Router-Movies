bbcbcea4bfaabc4de9dcea55da94947970c9a83a
Josh Cirre <joshcirre@gmail.com>
2019-10-30 17:05:12 -0700

    implemented a bug fix so that duplicate movies can't be saved :bug:

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
 import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
-
   const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
+    !savedList.find(savedMovie => savedMovie.title === movie.title) &&
+      setSavedList([...savedList, movie]);
     console.log(savedList);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <Route exact path='/' component={MovieList} />
+      <Route path='/' exact component={MovieList} />
       <Route
         path='/movie/:id'
         render={props => <Movie {...props} addToSavedList={addToSavedList} />}
       />
+      <Redirect to='/' />
     </div>
   );
 };
